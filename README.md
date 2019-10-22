fdedbae7d84d5c5d8d2cb1be3933aee59d0d3f89
olesyamogil <olesya.mogil@gmail.com>
2019-10-22 15:46:15 +0300

    Solution

--- README.md
+++ README.md
@@ -15,4 +15,4 @@ Requirements:
 
 **Result**
 
-- [UPDATE DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+- [DEMO LINK](https://olesyamogil.github.io/js_all-is-timed-DOM/)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const timer = document.getElementById('timer');
+let timeSpent = localStorage.getItem('time') || 0;
+timer.textContent = timeSpent;
+
+setInterval(() => {
+  timeSpent++;
+  localStorage.setItem('time', timeSpent);
+  timer.textContent = timeSpent;
+}, 1000);
